4219e6dca3a2b8ca538ba18de65cc2cf35402d20
jasonzhang1986 <jifengzhang.barlow@gmail.com>
2018-01-18 19:00:48 +0800

    代码提交

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ### 1. [Numpy 教程](https://wizardforcel.gitbooks.io/ts-numpy-tut/content/0.html)
 ### 2. [100 numpy exercises](http://www.labri.fr/perso/nrougier/teaching/numpy.100/)
 ### 3. [Python for Data Analysis, 2nd Edition](https://github.com/BrambleXu/pydata-notebook/blob/master/README.md)
+### 4. [抓取豆瓣电影Top250](douban/douban_top250.md)
---
+++ douban/douban_top250.py
@@ -0,0 +1,134 @@
+
+# coding: utf-8
+
+# In[25]:
+
+
+import requests
+from lxml import html
+import re
+import csv
+
+#解析html网页
+def parse_tree(url):
+    r = requests.get(url)
+    tree = html.fromstring(r.text)
+    return tree
+
+
+# In[55]:
+
+
+#获取title
+def get_title(tree):
+    titles = tree.xpath('//*[@id="content"]/div/div[1]/ol/*/div/div[2]/div[1]/a/span[1]/text()')
+    arr_title = []
+    # print(titles)
+    for title in titles:
+        arr_title.append(str(title))
+#     print(arr_title)
+    return arr_title
+
+
+# In[56]:
+
+
+#获取导演和主演，只取第一个名字
+def get_da(tree):
+    doctor = tree.xpath('//*[@id="content"]/div/div[1]/ol/*/div/div[2]/div[2]/p[1]/text()[1]')
+    da = []
+    for d in doctor:
+#         print(d)
+        dd = re.split(r'[\s\\xa0]', str(d))
+#         print(dd)
+        dactor = ''
+        actor = ''
+        for s in dd:
+            if '导演' in s:
+                index1 = dd.index(s)
+                if index1+1 < len(dd):
+                    dactor = dd[index1+1]
+            elif '主演' in s:
+                index = dd.index(s)
+                if index+1 < len(dd):
+                    actor = dd[index+1]
+                break
+        arr = [dactor, actor]
+        da.append(arr)
+#     print(da)
+    return da
+
+
+# In[57]:
+
+
+#获取年份
+def get_years(tree):
+    years = tree.xpath('//*[@id="content"]/div/div[1]/ol/*/div/div[2]/div[2]/p[1]/text()[2]')
+    arr_year = []
+    for y in years:
+        year = str(y).strip('\n').strip().split('\xa0/\xa0')
+        arr_year.append(year)
+#     print(arr_year)
+    return arr_year
+
+
+# In[58]:
+
+
+#获取评分
+def get_rate(tree):
+    rates = tree.xpath('//*[@id="content"]/div/div[1]/ol/*/div/div[2]/div[2]/div/span[2]/text()')
+    arr_rate = []
+    # print(rates)
+    for r in rates:
+        arr_rate.append(str(r))
+#     print(arr_rate)
+    return arr_rate
+
+
+# In[59]:
+
+
+#将结果拼接到result中
+def append(result,arr_title, arr_da, arr_year, arr_rate):
+    for i in range(len(arr_title)):
+        a = (arr_title[i], arr_da[i][0],arr_da[i][1], arr_year[i][0],arr_year[i][1],arr_year[i][2], arr_rate[i])
+        result.append(a)
+
+
+
+# In[68]:
+
+
+result_title = ['名字', '导演', '主演', '年份', '国家', '类型', '评分']
+result = []
+
+for i in range(10): #一共10页
+    count = i * 25 #每页25条数据
+    url = 'https://movie.douban.com/top250?start=%d' %(count) #拼接每页的url地址
+    print(url)
+    tree = parse_tree(url) #解析html
+    #拼接数据到result中
+    append(result,get_title(tree), get_da(tree), get_years(tree), get_rate(tree))
+
+#输出一下看看结果
+print(result_title)
+for r in result:
+    print(r) 
+
+#保存到csv中， newline字段是设置换行，默认是'\n'，会直接在csv中插入空行
+csvfile = open('douban_top250.csv', 'w', encoding='gb18030', newline='')
+writer = csv.writer(csvfile)
+#先写入title
+writer.writerow(result_title)
+print('====================')
+for r in result:
+    print(r)
+    #写入每一条数据，如果出错则跳过该条数据
+    try:
+        writer.writerow(r)
+    except:
+        continue
+csvfile.close() #最后关闭文件
+
